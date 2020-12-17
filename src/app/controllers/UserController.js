37474548096cadcfe778e53bfb3a929a6b49f835
Weslley de Campos <weslley082@gmail.com>
2020-12-17 11:09:33 -0300

    Crud usuario done

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,7 +22,7 @@ class UserController{
 
   async create (req,res){
     const schema = Yup.object().shape({
-      nome: Yup.string().required(),
+      name: Yup.string().required(),
       email: Yup.string().email('Email inválido').required(),
       senha: Yup.string().min(6).required(),
     });
@@ -30,17 +30,66 @@ class UserController{
       return res.status(400).json({msg : "Corpo da requisição inválido"});
     }
     try{
-      const { nome, email, senha } = req.body;
-      const password = await bcrypt.hash(senha, 8);
+      const { name, email, senha } = req.body;
+
 
       const usuarioRegistrado = await User.findOne({where :{ email }});
-      if(alunoRegistrado){
+      if(usuarioRegistrado){
         return res.status(400).json({msg:"Usuário já registrado"})
       }
+      const password = await bcrypt.hash(senha, 8);
+      const {id, name, email } = await User.create({
+        name,
+        email,
+        password_hash:password
+      });
+      const token = await jwt.sign({ id_aluno }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      });
 
-      const
+      return res.status(200).json({aluno:{id, name, email}, token});
     }catch(err){
       return res.status(400).json({msg:"Err"});
+    }
+  }
+  async update(req,res){
+    const schema = Yup.object().shape({
+      email: Yup.string().email(),
+      nome: Yup.string(),
+      senhaAntiga: Yup.string().min(6),
+      senha: Yup.string()
+        .min(6)
+        .when('oldPassword', (senhaAntiga, field) =>
+          senhaAntiga ? field.required() : field
+        ),
+      confirmacaoSenha: Yup.string().when('senha', (senha, field) =>
+        senha ? field.required().oneOf([Yup.ref('senha')]) : field
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        msg: 'Corpo da requisição inválido ou as senha são diferentes',
+      });
+    }
+    try{
+      const { id } = req.params;
+      const user= await User.findByPk(id);
+      if(!user){
+        return res.status(404).json({msg:"Usuário não encontrado"});
+      }
+      const { senhaAntiga, senha } = req.body;
+      if (senhaAntiga && !(await aluno.checkPassword(senhaAntiga))) {
+        return res.status(400).json({ msg: 'As senhas são diferentes' });
+      }
+
+      if(senha){
+        const senhaNova = await bcrypt.hash(senha, 8);
+        req.body.senha = senhaNova;
+      }
+      await user.update(req.body);
+      return res.status(200).json({msg:"Usuario atualizado"});
+    }catch(e){
+      return res.status(400).json({msg:"Erro"})
     }
   }
 }
