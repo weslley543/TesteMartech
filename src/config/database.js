5ee07923f913fd30113db061eb5f149729f4070f
Weslley de Campos <weslley082@gmail.com>
2020-12-17 10:21:18 -0300

    Ajustando as conexoes com banco e iniciando a implementaçao das funcionalidades

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
-  database: 'gymcontrol',
+  password: 'weslley1234',
+  database: 'TesteMartech',
   define: {
     timestamps: true,
     underscored: true,
